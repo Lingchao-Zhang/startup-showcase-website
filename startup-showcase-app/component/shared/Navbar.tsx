3eab7636025e4545f1ed21ae26c40654c484c1ff
Lingchao-Zhang <108935399+Lingchao-Zhang@users.noreply.github.com>
2025-02-21 17:49:47 +1100

    Sign in with next auth (#1)
    * Auth function implemented:
    1. used next-auth for auth and github for auth provider.
    2. configured auth in auth.ts and route.ts.
    3. moved page.tsx under root directory.
    4. created a new layout.tsx page to determine the new layout.
    5. created a Navbar to display login function.
    
    * Added css configuration and style:
    1. added css style to Navbar.
    2. modified css config in tailwind.config.ts.
    3. added css style in globals.css.
    4. added new font files.
    5. added new font config in layout.tsx.

---
+++ startup-showcase-app/component/shared/Navbar.tsx
@@ -0,0 +1,42 @@
+
+import { auth, signIn, signOut } from "@/auth"
+import Image from "next/image"
+import Link from "next/link"
+
+const Navbar = async () => {
+    const session = await auth()
+    return(
+        <header className="mx-5 mt-2">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/Roselia_logo.jpg" height={80} width={183} alt="Rose-logo" />
+                </Link>
+                <div className="flex gap-5 text-black">
+                {
+                    session && session?.user ? 
+                    <>
+                        <span>{session?.user?.name}</span>
+                        <form action={async () => {
+                                "use server"
+                                await signOut({redirectTo: "/"})
+                            }}
+                            >
+                            <button type="submit">Sign Out</button>
+                        </form>
+                    </>
+                    : 
+                    <form action={async () => {
+                        "use server"
+                        await signIn("github")
+                    }}>
+                        <button type="submit">Sign in</button>
+                    </form>
+                }
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
+
